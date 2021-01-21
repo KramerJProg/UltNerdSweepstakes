8e5a3444746cb33ac042e3a8ae866faa0bff6c4d
Kramer Jensen <Kramerjensend@gmail.com>
2021-01-20 20:31:57 -0800

    Added Contestant object in controller

--- UltNerdSweepstakes/Controllers/ContestantController.cs
+++ UltNerdSweepstakes/Controllers/ContestantController.cs
@@ -1,15 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UltNerdSweepstakes.Models;
 
 namespace UltNerdSweepstakes.Controllers
 {
     public class ContestantController : Controller
     {
+        [HttpGet]
         public IActionResult Add()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(IFormCollection form)
+        {
+            // Contestant object
+            Contestant cont = new Contestant();
+            cont.FullName = form["full-name"];
+            cont.Email = form["email"];
+            cont.Phone = form["phone"];
+
+            // This is where we would add to the database!
+
+            ViewData["Added"] = cont.FullName + ", you are entered! Good Luck!";
+
             return View();
         }
     }
