2bcaaf7fe0d1e014c176e864736c2128ce2e2ac5
Kramer Jensen <Kramerjensend@gmail.com>
2021-01-20 16:45:09 -0800

    Added the ContestantController along with a view named Add

---
+++ UltNerdSweepstakes/Controllers/ContestantController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UltNerdSweepstakes.Controllers
+{
+    public class ContestantController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
